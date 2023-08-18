5fa559594c4ea5bf6fd3b35a41a6b6628f02cfe8
tdolan21 <timmydolan21@gmail.com>
2023-08-18 17:07:31 -0400

    added PostgreSQL and PGVector for vector search

--- app.py
+++ app.py
@@ -1,114 +1,82 @@
+from langchain import VectorDBQA
+from langchain.agents import AgentType, initialize_agent, load_tools, Tool, Agent
+from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
-from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
-from langchain import SerpAPIWrapper, LLMMathChain, Wikipedia
-from langchain.agents.tools import Tool
-from langchain.agents.react.base import DocstoreExplorer
-from langchain import OpenAI, Wikipedia
-from langchain.agents import initialize_agent, Tool
-from langchain.agents import AgentType
 from dotenv import load_dotenv
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores.pgvector import PGVector
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from pathlib import Path
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import DirectoryLoader
 import os
-import io
-import sys
+from langchain.document_loaders import WebBaseLoader
+from langchain.agents.agent_toolkits import create_vectorstore_agent, VectorStoreToolkit, VectorStoreInfo
+from typing import List, Tuple
+from langchain.docstore.document import Document
+from langchain.chat_models import ChatOpenAI
+from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
+from langchain.utilities.wolfram_alpha import WolframAlphaAPIWrapper
+from langchain import SerpAPIWrapper
+from langchain.agents.tools import Tool
+from langchain import LLMMathChain
+from langchain.memory import PostgresChatMessageHistory
+
 
 
 load_dotenv()
 
-# Initialize Components
-os.environ['OPENAI_API_KEY'] = ""
-serpapi_api_key = os.environ['SERPAPI_API_KEY'] = ""
 
-docstore = DocstoreExplorer(Wikipedia())
+
 search = SerpAPIWrapper()
-llm = OpenAI(temperature=0.7, model="gpt-3.5-turbo")
-llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+wolfram = WolframAlphaAPIWrapper()
+
+
+llm = OpenAI(temperature=0.7, model="gpt-3.5-turbo", streaming=True)
 tools = [
-    Tool(name="Search", func=search.run, description="useful for when you need to answer questions about current events"),
-    Tool(name="Calculator", func=llm_math_chain.run, description="useful for when you need to answer questions about math"),
+    Tool(
+        name = "Search",
+        func=search.run,
+        description="useful for when you need to answer questions about current events"
+    ),
+    Tool(
+        name="Wolfram",
+        func=wolfram.run,
+        description="useful for when you need to answer questions about math"
+    ),
+    
+    
 ]
-model = ChatOpenAI(temperature=0.7, model="gpt-3.5-turbo")
+
+history = PostgresChatMessageHistory(
+    connection_string=os.getenv("CONNECTION_STRING"),
+    session_id="16390",
+)
+
+
+model = ChatOpenAI(temperature=0)
 planner = load_chat_planner(model)
 executor = load_agent_executor(model, tools, verbose=True)
 agent = PlanAndExecute(planner=planner, executor=executor, verbose=True)
 
 
-def configuration_page():
-    st.title("Configuration")
-    st.sidebar.subheader('Tools Selection')
     
 
-def chat_page():
-    st.title('💬 Chat with miniAGI')
+if "shared" not in st.session_state:
+   st.session_state["shared"] = True
 
-    # Redirect standard output to a custom StringIO object
-    original_stdout = sys.stdout
-    new_stdout = io.StringIO()
-    sys.stdout = new_stdout
+st.title("miniAGI :computer:")
+st.subheader("AGI with more targeted toolkits with decision making based on the plan and execution model from langchain")
 
-    # Radio button to select agent type
-    selected_agent = st.radio("Select Agent", ["Plan and Execute", "ReAct"])
-
-    # Input field for user's question
-    user_input = st.text_input("Ask a question:")
-
-    # Button to trigger the agent's response
-    if st.button('Ask'):
-        # Add user input to display
-        st.write(f"You: {user_input}")
-
-        # Determine response based on selected agent
-        if selected_agent == "Plan and Execute":
-            response = agent.run(user_input)
-        else:
-            llm = OpenAI(temperature=0.7, model_name="gpt-3.5-turbo")  
-            tools = [
-                Tool(
-        name="Search",
-        func=docstore.search,
-        description="useful for when you need to ask with search",
-    ),
-                Tool(
-        name="Lookup",
-        func=docstore.lookup,
-        description="useful for when you need to ask with lookup",
-    ),
-]           
-            react = initialize_agent(tools, llm, agent=AgentType.REACT_DOCSTORE, verbose=True)
-            response = react.run(user_input)
-
-        # Add agent response to display
-        st.write(f"Agent: {response}")
-
-        # Restore standard output
-        sys.stdout = original_stdout
-
-        # Get the console output from the custom StringIO object
-        console_output = new_stdout.getvalue()
-
-        # Add console output to display if there's any
-        if console_output.strip():
-            st.subheader("Console Output:")
-            st.code(console_output.strip())
-
-    # Clear the custom StringIO object for the next interaction
-    new_stdout.truncate(0)
-    new_stdout.seek(0)
-
-
-
-
-def main():
-    # Create sidebar menu
-    page_selection = st.sidebar.radio("Navigate:", [ "Chat with miniAGI", "Configuration"])
-
-    if page_selection == "Configuration":
-        configuration_page()
-    elif page_selection == "Chat with miniAGI":
-        chat_page()
-    
-
-if __name__ == '__main__':
-    main()
-
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
+    history.add_user_message(prompt)
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(prompt, callbacks=[st_callback])
+        st.write(response)
+        history.add_ai_message(response)
+        
---
+++ pages/huggingface_agi.py
@@ -0,0 +1,52 @@
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain
+import streamlit as st
+from langchain.callbacks import StreamlitCallbackHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+repo_id = "google/flan-t5-xxl"
+
+
+
+template = """Question: {prompt}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["prompt"])
+
+llm = HuggingFaceHub(
+    repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 64}
+)
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+
+st.title("HuggingFace AGI :computer:")
+
+repo_id = st.selectbox('Please select a model:',(
+                  "google/flan-t5-xxl",
+                  "databricks/dolly-v2-3b",
+                  "Writer/camel-5b-hf",
+                  "Salesforce/xgen-7b-8k-base",
+                  "tiiuae/falcon-40b",
+                  "TheBloke/Wizard-Vicuna-30B-Uncensored-GGML"
+            ))
+
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
+
+    with st.chat_message("assistant"):
+            st_callback = StreamlitCallbackHandler(st.container())
+
+            
+
+            template = """Question: {prompt}
+
+            Answer: Let's think step by step."""
+
+            prompt = PromptTemplate(template=template, input_variables=["prompt"])
+            
+            st.write(llm_chain.run(prompt))
